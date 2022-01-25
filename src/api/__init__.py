4069474d9f92dcc7ea2bd222838e0d6db742022c
Marc Fiani <mnh.2403@gmail.com>
2022-01-25 11:46:42 -0500

    completing the demo notebook

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,3 +1,4 @@
 from .get_instruments import get_instruments
+from .get_historical_data import get_historical_data
 # from .stringToLower import stringToLower
 # from .stringToUpper import stringToUpper
